c78c10786a9dd8de15434853418730f747c75565
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-08 21:36:08 +0530

    Config: Add `webpack` config -> `dev/prod/common`

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: "Production",
+    }),
+  ],
+};

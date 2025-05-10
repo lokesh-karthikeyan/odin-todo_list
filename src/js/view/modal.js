8fe58d087f2772d1b0174de76c1441d86272d839
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-10 14:14:14 +0530

    Feat: Add `modal form` to `edit` todo items

--- src/js/view/modal.js
+++ src/js/view/modal.js
@@ -1,23 +1,62 @@
 const modalForm = (function () {
   const modalContainer = document.querySelector(".modal-container");
   const projectForm = document.querySelector(".modal.project");
+  const todoForm = document.querySelector(".modal.todo");
+  const editTodoForm = document.querySelector(".modal.edit-todo");
   const newProject = document.querySelector(".project-button");
+  const newTodo = document.querySelector(".todo-button");
 
   newProject.addEventListener("click", () => {
-    openModal();
+    openProjectModal();
   });
 
-  const openModal = () => {
+  newTodo.addEventListener("click", () => {
+    openTodoModal();
+  });
+
+  const openProjectModal = () => {
     modalContainer.classList.toggle("active");
     projectForm.showModal();
   };
 
-  const closeModal = () => {
+  const openTodoModal = () => {
     modalContainer.classList.toggle("active");
-    projectForm.close();
+    todoForm.showModal();
   };
 
-  return { closeModal };
+  const openEditTodoModal = () => {
+    modalContainer.classList.toggle("active");
+    editTodoForm.showModal();
+  };
+
+  const closeProjectModal = () => {
+    modalContainer.classList.toggle("active");
+    projectForm.close();
+    document.getElementById("project-name").value = "";
+  };
+
+  const closeTodoModal = () => {
+    modalContainer.classList.toggle("active");
+    todoForm.close();
+    document.getElementById("todo-title").value = "";
+    document.getElementById("todo-description").value = "";
+    document.getElementById("todo-dueDate").value = "";
+  };
+
+  const closeEditTodoModal = () => {
+    modalContainer.classList.toggle("active");
+    editTodoForm.close();
+    document.getElementById("todo-edit-title").value = "";
+    document.getElementById("todo-edit-description").value = "";
+    document.getElementById("todo-edit-dueDate").value = "";
+  };
+
+  return {
+    closeProjectModal,
+    closeTodoModal,
+    openEditTodoModal,
+    closeEditTodoModal,
+  };
 })();
 
 export default modalForm;
